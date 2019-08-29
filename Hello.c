b97dfabef02e1e761bf0f448e49b16551dafe274
Pascal <ntagandapascal3@gmail.com>
2019-08-29 09:56:35 -0500

    Create Hello.c
    Added a script to print my major

---
+++ Hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Pascal Ntaganda
+ * Date: 2019/09/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Pascal_Ntaganda\");
+  printf("Integrated_science", );
+
+  return 0;
+}
